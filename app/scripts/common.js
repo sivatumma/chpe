81eb7f9483ae457ec0b8c5331e37e754a01985ae
siva.php5@gmail.com <siva.php5@gmail.com>
2015-09-14 17:40:34 +0530

    folder sync, merge

--- app/scripts/common.js
+++
@@ -1,9 +0,0 @@
-var toIsoString = function(str){
-        var date = new Date();
-            if(str){
-                date = new Date(str);
-                return date.toISOString();
-            }else{
-                return date.toISOString();
-            }
-    };
