0e46b057f9d3842ddac3455c85ac25985409350e
unknown <Inthiyaz.karamala@CALLHEALTH3206.CHSPLDC.COM>
2015-10-20 17:49:24 +0530

    msg

--- server/config/queryBuilder.js
+++ server/config/queryBuilder.js
@@ -3,21 +3,43 @@ var _ = require('lodash');
 
 module.exports = {
 	calculateSLDiscounts: function(serviceLevelDiscounts, bodydata) {
-		console.log("Inside calculateSLDiscounts module method", serviceLevelDiscounts, bodydata.servicesAmount);
 		var servicelevel = _.filter(serviceLevelDiscounts, function(item) {
 			var finalservice = _.intersection(item._services, bodydata.services);
 			if (finalservice.length == item._services.length)
 				return item;
 		});
-		console.log("This is the servicelevel", servicelevel);
-		var discountAmount = chUtils.callDiscount(bodydata.servicesAmount, servicelevel[0].discount, servicelevel[0].discountType)
-		console.log(discountAmount);
+		var discountAmount = chUtils.callDiscount(bodydata.servicesAmount, servicelevel[0].discount, servicelevel[0].discountType);
 		return discountAmount;
 	},
-	calculateSRCDiscounts: function(serviceLevelDiscounts, servicesAmount) {
+	calculateSRCDiscounts: function(SRCData,SRCAmount,SRCServices) {
+		var SRCDiscount = _.filter(SRCData, function(item) {
+			var finalservice = _.intersection(item._srcTypes, SRCServices);
+			if (finalservice.length == item._srcTypes.length)
+				return item;
+		});
+		var discountAmount = chUtils.callDiscount(SRCAmount, SRCDiscount[0].discount, SRCDiscount[0].discountType);
 		return discountAmount;
 	},
-	calculateDLDiscounts: function(serviceLevelDiscounts, servicesAmount) {
+	calculateDLDiscounts: function(DLData, DLAmount, DCType) {
+		var doctorDiscount = _.filter(DLData, function(item) {
+			if (item.type == DCType) return item;
+		});
+		var discountAmount = chUtils.callDiscount(DLAmount, doctorDiscount[0].discount, doctorDiscount[0].discountType);
+		return discountAmount;
+	},
+	calcualateMOP: function(MOPData, MOPAmount, MOPType) {
+		var MOPDiscount = _.filter(MOPData, function(item) {
+			if (item.MOP == MOPType) return item;
+		});
+		var discountAmount = chUtils.callDiscount(MOPAmount, MOPDiscount[0].discount, MOPDiscount[0].discountType);
+		return discountAmount;
+
+	},
+	calculateBVDiscount : function(BLSData,finalAmount)
+	{
+     var BLSDiscount =  _.filter(BLSData, function(item){ if(finalAmount > item.billRange.from && finalAmount < item.billRange.to) return item;});
+     var discountAmount = chUtils.callDiscount(finalAmount,BLSData[0].discount,BLSData[0].discountType);
+     return discountAmount;
 	},
 	createSchema: function(data) {
 		return data;
@@ -60,24 +82,33 @@ module.exports = {
 		return query;
 	},
 	saveOrder: function(bodydata, data) {
+		var finalData ={};
 		console.log("Inside saveOrder");
 		if (data[0].metadata.type in ["GIFT_CARD", "COUPON"]) {
 			bodydata.finalAmount = chUtils.callDiscount(bodydata.billAmount, data[0].behavior.discount, data[0].behavior.discountType);
 		} else if (data[0].metadata.type == "ADD_ON") {
-			this.calculateSLDiscounts(data[0].behavior.serviceLevelDiscounts, bodydata);
-		}
+		finalData.billAmount    = parseInt(bodydata.servicesAmount) + parseInt( bodydata.doctorAmount) + parseInt(bodydata.SRCAmount);
+		finalData.SLDisocunt  = this.calculateSLDiscounts(data[0].behavior.serviceLevelDiscounts, bodydata);
+        finalData.DLDiscount  = this.calculateDLDiscounts(data[0].behavior.doctorLevelDiscounts,bodydata.doctorAmount,bodydata.doctorChoiceType);
+	    finalData.SRCDiscount = this.calculateSRCDiscounts(data[0].behavior.serviceRateCategoryDiscounts,bodydata.SRCAmount,bodydata.SRCServices);
+	    var finalAmount       = parseInt(finalData.billAmount) - parseInt(finalData.SLDisocunt)- parseInt(finalData.DLDiscount)- parseInt(finalData.SRCDiscount);
+        finalData.BLS        = this.calculateBVDiscount(data[0].behavior.billValueDiscounts,finalAmount);
+        finalAmount          = finalData.billAmount  -  finalData.SLDisocunt -finalData.DLDiscount - finalData.BLS-finalData.SRCDiscount;
+        finalData.MOP        = this.calcualateMOP(data[0].behavior.modeOfPaymentDiscounts,finalAmount);
+		finalData.finalAmount = parseInt(finalAmount)- parseInt(finalData.MOP);
 
+		}
 		var query = {
 			schemeName: bodydata.name,
 			userId: bodydata.userId,
 			businessName: bodydata.businessName,
 			dateOfService: new Date(bodydata.dateOfService),
 			services: bodydata.services,
-			billAmount: bodydata.billAmount,
+			billAmount: finalData.billAmount,
 			successfullyApplied: false,
 			businessOrderId: bodydata.businessOrderId,
 			locationOfService: bodydata.orderLocationOfService,
-			finalAmount: bodydata.finalAmount
+			finalAmount:finalData.finalAmount
 		};
 		return query;
 	}
