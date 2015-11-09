ee59b90400375412a8e190e0168a93f1e3cf8a2e
sivakumar1.manda <siva.php5@gmail.com>
2015-11-09 11:50:22 +0530

    Overview element added

--- client/config/general.js
+++ client/config/general.js
@@ -3,6 +3,7 @@ function generalConfig() {
 		toUseApiHost:false,
 		apiHost: '',
 		rest: '/mdb/scheme?API_KEY=MEDIBUS-12ed15e7-bc20-45c5-88dc-684bb32a9dd9',
+		overview: '/pricingengine/overview?API_KEY=MEDIBUS-12ed15e7-bc20-45c5-88dc-684bb32a9dd9',
 		memcache: {
 			names:'/api/proxy?url=http://52.76.37.144:8080/javaapi/rest/names',
 			locations: '/api/proxy?url=http://52.76.37.144:8080/javaapi/rest/locations/',
