ec2556238d794081e4c25b576883e46468e9c372
sivakumar <siva.php5@gmail.com>
2015-11-18 15:31:04 +0530

    For Pull

--- client/config/general.js
+++ client/config/general.js
@@ -4,8 +4,9 @@ function generalConfig() {
 		apiHost: '',
 		rest: '/mdb/scheme?API_KEY=MEDIBUS-12ed15e7-bc20-45c5-88dc-684bb32a9dd9',
 		overview: '/pricingengine/overview?API_KEY=MEDIBUS-12ed15e7-bc20-45c5-88dc-684bb32a9dd9',
-		preview: '/pricingengine/previewData?API_KEY=MEDIBUS-12ed15e7-bc20-45c5-88dc-684bb32a9dd9',
+		preview: '/pricingengine/previewData?',
 		signout :'http://172.19.16.71:91/ssoLogout',
+		API_KEY:'MEDIBUS-12ed15e7-bc20-45c5-88dc-684bb32a9dd9',
 		memcache: {
 			names:'/api/proxy?url=http://52.76.37.144:8080/javaapi/rest/names',
 			locations: '/api/proxy?url=http://52.76.37.144:8080/javaapi/rest/locations/',
