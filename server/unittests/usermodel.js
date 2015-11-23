e437afcfa46a6306d8fd8b2fb4651989219bef60
unknown <Inthiyaz.karamala@CALLHEALTH3206.CHSPLDC.COM>
2015-11-23 15:46:27 +0530

    msg

--- server/unittests/usermodel.js
+++ server/unittests/usermodel.js
@@ -1,13 +1,28 @@
 var expect = require('expect.js');
-var user = require('../models/user.js');
 var mongoose = require('mongoose');
+var user = require('../models/user.js')(mongoose);
 mongoose.connect('mongodb://localhost/pricingEngine') ;
 describe('Model',function(){
 
-it('database field missing',function(){
-console.log(mongoose.model(user).usersSchema.username);
+it('User Model Field Name Missing',function(){
 
-expect(typeof user).to.be(typeof (mongoose.model));
+
+expect(user.schema.paths.username).to.be.ok();
+expect(user.schema.paths.password).to.be.ok();
+expect(user.schema.paths.active).to.be.ok();
+expect(user.schema.paths.loginAttempts).to.be.ok();
+expect(user.schema.paths.lockUntil).to.be.ok();
+expect(user.schema.paths.roles).to.be.ok();
+expect(user.schema.paths.roleCount).to.be.ok();
+expect(user.schema.paths.provider).to.be.ok();
+expect(user.schema.paths.created_at).to.be.ok();
+expect(user.schema.paths.updated_at).to.be.ok();
+expect(user.schema.paths.tokens).to.be.an( 'object' );
+
+mongoose.model('User')({});
+
+
+
 
 })
 
